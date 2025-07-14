5b4c91e78c378dd09b2fe771bb52098aaa57264b
kuum97 <kuum97@icloud.com>
2025-07-14 16:15:48 +0900

    etc: add schedule

--- src/app/RecordInput.tsx
+++ src/app/RecordInput.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 const RecordInput = () => {
+  // store의 상태와 액션 연결 => 아웃풋 구현단계와의 순서 고려
+  // 쿼리 전송 기반 코드 작성
   async function submitRecord(formData: FormData) {
     const query = formData.get("record-query");
 
